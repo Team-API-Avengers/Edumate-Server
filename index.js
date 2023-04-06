23f806b99356d3c34db054a61d58aa4b687c265b
siamalrafi <rahathasandipto@gmail.com>
2023-04-06 21:14:41 +0600

    next code

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const colors = require("colors");
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+const app = require("./app");
+
+// database connection
+// DBConnect(); initial code----
+
+// mongoose.connect('mongodb://localhost:27017/').then(() => {
+mongoose.connect(process.env.DATABASE).then(() => {
+    console.log("Database Connection successfully established".red.bold);
+});
+
+
+
+// ---------- Server PORT ----------
+const port = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+    res.send("EduMate server Running...");
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    console.log(`App is running on port ${port}`.yellow.bold);
+});
