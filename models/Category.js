cfe40783006a3059e221df1436b1da754057801e
Ashikzzzz <ashik.emon5.im@gmail.com>
2023-04-07 01:12:19 +0600

    last push

---
+++ models/Category.js
@@ -0,0 +1,13 @@
+// const mongoose = require('mongoose');
+
+// const categorySchema= new mongoose.Schema({
+//     background:{
+//         type: String,
+//         required: true,
+        
+//     }
+// });
+
+// const categoryModel = mongoose.model("categoryModel",categorySchema)
+
+// module.exports = categoryModel;
