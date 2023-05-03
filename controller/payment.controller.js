0566913ab075412c9ee2516c5ef119120f9d0b6f
Ashikzzzz <ashik.emon5.im@gmail.com>
2023-05-04 00:24:41 +0600

    test

--- controller/payment.controller.js
+++ controller/payment.controller.js
@@ -45,8 +45,8 @@ exports.createApayment =  async (req, res) => {
       sslcz.init(data).then(apiResponse => {
           // Redirect the user to payment gateway
           let GatewayPageURL = apiResponse.GatewayPageURL
-          res.send({url:GatewayPageURL})
-          console.log('Redirecting to: ', GatewayPageURL)
+        res.redirect(GatewayPageURL)
+        console.log('Redirecting to: ', GatewayPageURL)
       });
     }
   
