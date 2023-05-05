c8c6791485b3fe2944d037f1aaffa28918b213f6
siamalrafi <rahathasandipto>
2023-05-05 22:32:46 +0600

    this is the schema for post api

---
+++ models/post.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+
+const postSchema = new mongoose.Schema({
+    category: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    authorName: {
+        type: String,
+        required: true
+    },
+    authorRole: {
+        type: String,
+        required: true
+    },
+    authorImage: {
+        type: String,
+        required: true
+    },
+    authorEmail: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+}, {
+    timestamps: true
+})
+
+const Post = mongoose.model('Post', postSchema)
+
+// export  Post
+module.exports = Post;
